e9e70a9aff7de0ea2f58d9930c60eb8896d4d4c2
Djesse Dirckx <djessedirckx@gmail.com>
2018-09-17 20:59:20 +0200

    Implemented emotion with dlib

--- python_modules/detection/posture.py
+++
@@ -1,9 +0,0 @@
-import dlib
-
-
-def check_posture(rect):
-    #TODO replace with settings
-    settingrect = dlib.rectangle(120, 120, 280, 280)
-
-    return rect.tr_corner().x > settingrect.tr_corner().x or rect.tl_corner().x < settingrect.tl_corner().x or \
-           rect.br_corner().y > settingrect.br_corner().y or rect.tl_corner().y < settingrect.tl_corner().y
