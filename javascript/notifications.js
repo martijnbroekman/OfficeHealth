a353d791957a0441b7e9c05fc6a8532d8cdcfecf
JCL Reijbroek <jorn.reijbroek@hotmail.com>
2018-10-03 16:55:38 +0200

    Added visual design for new cross platform notifications

--- javascript/notifications.js
+++ javascript/notifications.js
@@ -1,5 +1,11 @@
+const electron = require('electron');
 const path = require('path');
 const notifications = require( 'freedesktop-notifications' );
+
+const {
+    BrowserWindow,
+    Menu
+} = electron;
 
 // Construct and push notification
 const pushNotification = (title, message) => {
@@ -41,7 +47,38 @@ const pushNotificationWithoutActions = (title, message) => {
     notif.push();
 };
 
+let notificationWindow = null;
+const createNotificationWindow = () => {
+    let notificationWidth = 320;
+    let notificationHeight = 320;
+    let xPosition = electron.screen.getPrimaryDisplay().bounds.width - notificationWidth - 15;
+    let yPosition = electron.screen.getPrimaryDisplay().bounds.height - notificationHeight - 15;
+    
+    notificationWindow = new BrowserWindow({
+        width: notificationWidth,
+        height: notificationHeight,
+        x: xPosition,
+        y: yPosition,
+        resizable: false,
+        frame: false,
+        icon: path.join(__dirname, '../icons/png/dark-icon-pngs/64x64.png')
+    });
+
+    notificationWindow.loadURL(require('url').format({
+        pathname: path.join(__dirname, '../html/notification.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    notificationWindow.on('closed', () => {
+        notificationWindow = null
+    });
+
+    Menu.setApplicationMenu(null);
+};
+
 module.exports = {
     PushNotification: pushNotification,
-    pushNotificationWithoutActions: pushNotificationWithoutActions
+    pushNotificationWithoutActions: pushNotificationWithoutActions,
+    createNotificationWindow: createNotificationWindow
 };
